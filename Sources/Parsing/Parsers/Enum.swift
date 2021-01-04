b9f5ad8397a93c6481f378753767fab6a801f1f9
Luke Redpath <lredpath@community.com>
2021-01-04 12:11:42 +0000

    Add support for parsing enum raw string values.

---
+++ Sources/Parsing/Parsers/Enum.swift
@@ -0,0 +1,55 @@
+extension Parsers {
+  /// Parses out the first matching raw value in a list of possible values from the start of the current input.
+  @inlinable
+  public static func enumRawValue<EnumType: RawRepresentable>(
+    in possibleValues: [EnumType]
+  ) -> AnyParser<Substring, EnumType> where EnumType.RawValue == String {
+    OneOfMany(
+      possibleValues.map { value in
+        StartsWith(value.rawValue[...]).map { value }
+      }
+    )
+    .eraseToAnyParser()
+  }
+}
+
+/// Provides an opt-in convenience high-level API for parsing enum values.
+public protocol CaseParsable: RawRepresentable {}
+
+extension CaseParsable where Self.RawValue == String {
+  /// Parses out the first matching raw value from a list of possible values, or returns nil.
+  ///
+  /// Example:
+  ///
+  ///     enum SomeValues: String, CaseParseable {
+  ///         case one
+  ///         case two
+  ///     }
+  ///
+  ///     SomeValues.firstMatchingValue(in: [.one]).parse("one").output // SomeValues.one
+  ///     SomeValues.firstMatchingValue(in: [.two]).parse("one").output // nil
+  ///
+  public static func firstMatchingCase(
+    in possibleValues: [Self]
+  ) -> AnyParser<Substring, Self> {
+    Parsers.enumRawValue(in: possibleValues)
+  }
+}
+
+extension CaseParsable where Self: CaseIterable, Self.RawValue == String {
+  /// Parses out the first matching raw value from all possible cases, or returns nil.
+  ///
+  /// Example:
+  ///
+  ///     enum SomeValues: String, CaseParseable, CaseIterable {
+  ///         case one
+  ///         case two
+  ///     }
+  ///
+  ///     SomeValues.firstMatchingValue.parse("one").output // SomeValues.one
+  ///     SomeValues.firstMatchingValue.parse("six").output // nil
+  ///
+  public static var firstMatchingCase: AnyParser<Substring, Self> {
+    Parsers.enumRawValue(in: allCases as? [Self] ?? [])
+  }
+}
