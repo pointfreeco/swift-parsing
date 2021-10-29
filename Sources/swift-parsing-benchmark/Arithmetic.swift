f13715d8a0048d028ed6af0ffcf2f213ff4deb09
Stephen Celis <stephen@stephencelis.com>
2021-10-29 09:12:49 -0400

    wip

--- Sources/swift-parsing-benchmark/Arithmetic.swift
+++ Sources/swift-parsing-benchmark/Arithmetic.swift
@@ -1,61 +1,96 @@
 import Benchmark
 import Parsing
 
-private var expr: AnyParser<Substring.UTF8View, Int> {
-  Lazy { term }.chainl1(
-    symbol("+").take(Always(+))
-      .orElse(symbol("-").take(Always(-)))
-  )
-  .eraseToAnyParser()
-}
-
-private var term: AnyParser<Substring.UTF8View, Int> {
-  factor.chainl1(
-    symbol("*").take(Always(*))
-      .orElse(symbol("/").take(Always(/)))
-  )
-  .eraseToAnyParser()
-}
-
-private let factor = symbol("(").take(Lazy { expr }).skip(symbol(")"))
-  .orElse(natural)
-
-private let natural = Skip(Whitespace<Substring.UTF8View>()).take(Int.parser()).skip(Whitespace())
-
-private let symbol = { (symbol: String) in
-  Skip(Whitespace<Substring.UTF8View>()).skip(StartsWith(symbol.utf8)).skip(Whitespace())
-}
-
-private func fix<A, B>(_ f: @escaping (@escaping (A) -> B) -> (A) -> B) -> (A) -> B {
-  return { f(fix(f))($0) }
-}
-
-extension Parser {
-  fileprivate func chainl1<P>(_ op: P) -> AnyParser<Input, Output>
-  where P: Parser, P.Input == Input, P.Output == (Output, Output) -> Output {
-    self.flatMap { x in
-      fix { recur in
-        { x in
-          op.flatMap { f in
-            self.flatMap { y in
-              recur(f(x, y))
-                .eraseToAnyParser()
-            }
-          }
-          .orElse(Always(x))
-          .eraseToAnyParser()
-        }
-      }(x)
-    }
-    .eraseToAnyParser()
-  }
-}
-
 let arithmeticSuite = BenchmarkSuite(name: "Arithmetic") { suite in
   let arithmetic = "1 + 2 * 3 / 4 - 5"
 
   suite.benchmark("Parser") {
     var a = arithmetic[...].utf8
     precondition(expr.parse(&a) == -3)
+  }
+}
+
+private var expr: AnyParser<Substring.UTF8View, Int> {
+  AnyParser(
+    InfixLeft {
+      Skip { Whitespace() }
+      OneOf {
+        "+".utf8.map { (+) }
+        "-".utf8.map { (-) }
+      }
+      Skip { Whitespace() }
+    } expression: {
+      Lazy { term }
+    }
+  )
+}
+
+private let term = InfixLeft {
+  Skip { Whitespace() }
+  OneOf {
+    "*".utf8.map { (*) }
+    "/".utf8.map { (/) }
+  }
+  Skip { Whitespace() }
+} expression: {
+  factor
+}
+
+private let factor = OneOf {
+  Parse {
+    Skip { "(".utf8 }
+    expr
+    Skip { ")".utf8 }
+  }
+  natural
+}
+
+private let natural = Parse {
+  Skip { Whitespace() }
+  Int.parser()
+  Skip { Whitespace() }
+}
+
+/*
+ TODO: Should this be:
+
+ InfixOperator(associativity: .left) {
+   OneOf {
+     "+".map(+)
+     "-".map(-)
+   }
+ } expression {
+   Lazy { term }
+ }
+ */
+private struct InfixLeft<Operator, Expression>: Parser
+where
+  Operator: Parser,
+  Expression: Parser,
+  Operator.Input == Expression.Input,
+  Operator.Output == (Expression.Output, Expression.Output) -> Expression.Output
+{
+  let expression: Expression
+  let `operator`: Operator
+
+  @inlinable
+  init(
+    @ParserBuilder _ operator: () -> Operator,
+    @ParserBuilder expression: () -> Expression
+  ) {
+    self.expression = expression()
+    self.operator = `operator`()
+  }
+
+  @inlinable
+  func parse(_ input: inout Expression.Input) -> Expression.Output? {
+    guard var lhs = self.expression.parse(&input) else { return nil }
+    while
+      let operation = self.operator.parse(&input),
+      let rhs = self.expression.parse(&input)
+    {
+      lhs = operation(lhs, rhs)
+    }
+    return lhs
   }
 }
