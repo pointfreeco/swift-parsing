dadb950cf73f8df0e3a091397d70b27fa467dbde
Stephen Celis <stephen@stephencelis.com>
2021-11-23 17:49:49 -0500

    wip

---
+++ Sources/Parsing/ParserPrinters/Exactly.swift
@@ -0,0 +1,23 @@
+public struct Exactly<Output>: ParserPrinter
+where
+  Output: Equatable
+{
+  public let output: Output
+
+  @inlinable
+  public init(
+    _ output: Output
+  ) where Output: Equatable {
+    self.output = output
+  }
+
+  @inlinable
+  public func parse(_ input: inout Void) -> Output? {
+    self.output
+  }
+
+  @inlinable
+  public func print(_ output: Output) -> Void? {
+    self.output == output ? () : nil
+  }
+}
