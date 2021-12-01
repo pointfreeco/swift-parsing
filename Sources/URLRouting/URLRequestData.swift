fe14b606f1ea55e3d6489de17f38219bfbe0069a
Stephen Celis <stephen@stephencelis.com>
2021-11-30 20:52:50 -0500

    wip

--- Sources/URLRouting/URLRequestData.swift
+++ Sources/URLRouting/URLRequestData.swift
@@ -1,25 +1,39 @@
-public struct URLRequestData: Equatable { // TODO: Codable & Hashable
+public struct URLRequestData: Equatable {
   public var body: ArraySlice<UInt8>?
   public var headers: Fields
+  public var host: String?
   public var method: String?
+  public var password: String?
   public var path: ArraySlice<Substring>
+  public var port: Int?
   public var query: Fields
+  public var scheme: String?
+  public var user: String?
 
   public init(
     method: String? = nil,
+    scheme: String? = nil,
+    user: String? = nil,
+    password: String? = nil,
+    host: String? = nil,
+    port: Int? = nil,
     path: ArraySlice<Substring> = [],
     query: Fields = [:],
     headers: Fields = [:],
     body: ArraySlice<UInt8>? = nil
   ) {
+    self.body = body
+    self.headers = headers
+    self.host = host
     self.method = method
+    self.password = password
     self.path = path
     self.query = query
-    self.headers = headers
-    self.body = body
+    self.scheme = scheme
+    self.user = user
   }
 
-  public struct Fields: Equatable { // TODO: Codable & Hashable
+  public struct Fields: Equatable {
     public var fields: [String: ArraySlice<Substring?>]
 
     public init(_ fields: [String: ArraySlice<Substring?>]) {
@@ -48,6 +62,65 @@ extension URLRequestData: Appendable {
   }
 }
 
+extension URLRequestData: Codable {
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    self.body = try container.decodeIfPresent([UInt8].self, forKey: .body)?[...]
+    self.headers = try container.decodeIfPresent(Fields.self, forKey: .headers) ?? [:]
+    self.host = try container.decodeIfPresent(String.self, forKey: .host)
+    self.method = try container.decodeIfPresent(String.self, forKey: .method)
+    self.password = try container.decodeIfPresent(String.self, forKey: .password)
+    self.path = try container.decodeIfPresent([String].self, forKey: .path)?.map { $0[...] }[...]
+      ?? []
+    self.port = try container.decodeIfPresent(Int.self, forKey: .port)
+    self.query = try container.decodeIfPresent(Fields.self, forKey: .query) ?? [:]
+    self.scheme = try container.decodeIfPresent(String.self, forKey: .scheme)
+    self.user = try container.decodeIfPresent(String.self, forKey: .user)
+  }
+
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    try container.encodeIfPresent(self.body.map(Array.init), forKey: .body)
+    if !self.headers.isEmpty { try container.encode(self.headers, forKey: .headers) }
+    try container.encodeIfPresent(self.host, forKey: .host)
+    try container.encodeIfPresent(self.method, forKey: .method)
+    try container.encodeIfPresent(self.password, forKey: .password)
+    if !self.path.isEmpty { try container.encode(self.path.map(String.init), forKey: .path) }
+    try container.encodeIfPresent(self.port, forKey: .port)
+    if !self.query.isEmpty { try container.encode(self.query, forKey: .query) }
+    try container.encodeIfPresent(self.scheme, forKey: .scheme)
+    try container.encodeIfPresent(self.user, forKey: .user)
+  }
+
+  private enum CodingKeys: CodingKey {
+    case body
+    case headers
+    case host
+    case method
+    case password
+    case path
+    case port
+    case query
+    case scheme
+    case user
+  }
+}
+
+extension URLRequestData: Hashable {
+  public func hash(into hasher: inout Hasher) {
+    hasher.combine(self.body)
+    hasher.combine(self.method)
+    hasher.combine(self.headers)
+    hasher.combine(self.host)
+    hasher.combine(self.password)
+    hasher.combine(self.path)
+    hasher.combine(self.port)
+    hasher.combine(self.query)
+    hasher.combine(self.scheme)
+    hasher.combine(self.user)
+  }
+}
+
 extension URLRequestData.Fields {
   public subscript(name: String) -> ArraySlice<Substring?>? {
     _read { yield self.fields[name] }
@@ -69,6 +142,18 @@ extension URLRequestData.Fields: Appendable {
 
   public mutating func append(contentsOf other: URLRequestData.Fields) {
     self.fields.merge(other.fields, uniquingKeysWith: +)
+  }
+}
+
+extension URLRequestData.Fields: Codable {
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.singleValueContainer()
+    self.init(try container.decode([String: [String?]].self).mapValues { $0.map { $0?[...] }[...] })
+  }
+
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.singleValueContainer()
+    try container.encode(self.fields.mapValues { $0.map { $0.map(String.init) } })
   }
 }
 
@@ -96,5 +181,11 @@ extension URLRequestData.Fields: Collection {
 extension URLRequestData.Fields: ExpressibleByDictionaryLiteral {
   public init(dictionaryLiteral elements: (String, ArraySlice<Substring?>)...) {
     self.init(.init(uniqueKeysWithValues: elements))
+  }
+}
+
+extension URLRequestData.Fields: Hashable {
+  public func hash(into hasher: inout Hasher) {
+    hasher.combine(self.fields)
   }
 }
