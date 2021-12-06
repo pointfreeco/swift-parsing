80972f9e2537188ac86c3348f2f797e48109a73d
Stephen Celis <stephen@stephencelis.com>
2021-12-06 14:38:07 -0500

    wip

--- Sources/Parsing/Internal/Breakpoint.swift
+++ Sources/Parsing/Internal/Breakpoint.swift
@@ -1,10 +1,12 @@
-import Foundation
+#if canImport(Darwin)
+  import Darwin
+#endif
 
 /// Raises a debug breakpoint if a debugger is attached.
 @inline(__always)
 @usableFromInline
 func breakpoint(_ message: @autoclosure () -> String = "") {
-  #if DEBUG
+  #if canImport(Darwin)
     // https://github.com/bitstadium/HockeySDK-iOS/blob/c6e8d1e940299bec0c0585b1f7b86baf3b17fc82/Classes/BITHockeyHelper.m#L346-L370
     var name: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
     var info: kinfo_proc = kinfo_proc()
@@ -30,5 +32,7 @@ func breakpoint(_ message: @autoclosure () -> String = "") {
       )
       raise(SIGTRAP)
     }
+  #else
+    assertionFailure(message)
   #endif
 }
