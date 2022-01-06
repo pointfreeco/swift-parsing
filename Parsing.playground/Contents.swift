1efb5d7799ebfb8e1c9be6b6f3c7322d36ccab8c
Point-Free <pointfreeco@Stephens-M1-Pro.home>
2022-01-06 11:24:50 -0500

    wip

--- Parsing.playground/Contents.swift
+++ Parsing.playground/Contents.swift
@@ -1,24 +1,91 @@
 import Parsing
 
-struct Color {
-  let red, green, blue: UInt8
+// (inout Input) -> Output?
+
+var input = "123 Hello World!"[...]
+Int.parser().parse(&input)
+input
+
+//StartsWith(" Hello ").parse(&input)
+" Hello ".parse(&input)
+input
+
+Prefix
+  .init(while: { $0 != "!" }).parse(&input)
+input
+
+".".parse(&input)
+input
+
+input = "123 Hello World!"[...]
+Int.parser()
+  .skip(" Hello ")
+  .take(Prefix { $0 != "!" })
+  .skip("!")
+  .parse(&input)
+input
+
+input = """
+1,Blob,member
+2,Blob Jr,guest
+3,Blob Sr,admin
+"""[...]
+
+enum Role {
+  case admin, guest, member
 }
 
-let hexPrimary = Prefix(2)
-  .compactMap { UInt8($0, radix: 16) }
+let role = "admin".map { Role.admin }
+  .orElse("guest".map { Role.guest })
+  .orElse("member".map { Role.member })
 
-let hexColor = "#"
-  .take(hexPrimary)
-  .take(hexPrimary)
-  .take(hexPrimary)
-  .map(Color.init)
-
-do {
-  var hex = "#000000"[...]
-  print(hexColor.parse(&hex)!)
+struct User {
+  var id: Int
+  var name: String
+  var role: Role
 }
 
-do {
-  var hex = "#FF0000"[...]
-  print(hexColor.parse(&hex)!)
+let user = Int.parser()
+  .skip(",")
+  .take(Prefix { $0 != "," }.map(String.init))
+  .skip(",")
+  .take(role)
+  .map(User.init(id:name:role:))
+
+let users = Many(user, separator: "\n")
+users.parse(&input)
+
+input
+
+/*
+ Parsing.Many<Parsing.Parsers.Map<Parsing.Parsers.Take3<Parsing.Parsers.SkipSecond<Parsing.Parsers.Take2<Parsing.Parsers.SkipSecond<Parsing.Parsers.UTF8ViewToSubstring<Parsing.Parsers.IntParser<Substring.UTF8View, Int>>, String>, Parsing.Parsers.Map<Parsing.Prefix<Substring>, String>>, String>, Int, String, Parsing.Parsers.UTF8ViewToSubstring<Parsing.Parsers.BoolParser<Substring.UTF8View>>>, User>, Array<__lldb_expr_45.User>, String>
+ */
+
+
+import Combine
+
+let publisher = Just(1)
+  .map { $0 + 1 }
+  .flatMap { Just($0) }
+  .filter { $0.isMultiple(of: 2) }
+  .dropFirst()
+  .ignoreOutput()
+/*
+ Combine.Publishers.IgnoreOutput<Combine.Publishers.Drop<Combine.Publishers.Filter<Combine.Publishers.FlatMap<Combine.Just<Int>, Combine.Just<Int>>>>>
+ */
+
+import SwiftUI
+
+let view = Group {
+  ForEach(1...10, id: \.self) { index in
+    Button(action: {}) {
+      HStack {
+        Text("Number")
+        Text("\(index)")
+      }
+    }
+  }
 }
+/*
+ SwiftUI.ForEach<ClosedRange<Int>, Int, SwiftUI.Button<SwiftUI.HStack<SwiftUI.TupleView<(SwiftUI.Text, SwiftUI.Text)>>>>
+ */
