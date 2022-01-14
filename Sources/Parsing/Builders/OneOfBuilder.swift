be57b1f9dbb67295a3b51f388a6523db0a2f4cad
Stephen Celis <stephen@stephencelis.com>
2022-01-14 16:10:09 -0500

    wip

--- Sources/Parsing/Builders/OneOfBuilder.swift
+++ Sources/Parsing/Builders/OneOfBuilder.swift
@@ -1,7 +1,7 @@
 @resultBuilder
 public enum OneOfBuilder {
   @inlinable
-  public static func buildArray<P>(_ parsers: [P]) -> Parsers.OneOfMany<P> where P: Parser {
+  public static func buildArray<P>(_ parsers: [P]) -> Parsers.OneOfMany<P> {
     .init(parsers)
   }
 
@@ -13,33 +13,26 @@ public enum OneOfBuilder {
   @inlinable
   public static func buildEither<TrueParser, FalseParser>(
     first parser: TrueParser
-  ) -> Conditional<TrueParser, FalseParser>
-  where
-    TrueParser: Parser,
-    FalseParser: Parser
-  {
+  ) -> Conditional<TrueParser, FalseParser> {
     .first(parser)
   }
 
   @inlinable
   public static func buildEither<TrueParser, FalseParser>(
     second parser: FalseParser
-  ) -> Conditional<TrueParser, FalseParser>
-  where
-    TrueParser: Parser,
-    FalseParser: Parser
-  {
+  ) -> Conditional<TrueParser, FalseParser> {
     .second(parser)
   }
 
   @inlinable
-  public static func buildIf<P>(_ parser: P?) -> P? where P: Parser {
-    parser
+  public static func buildIf<P>(_ parser: P?) -> Conditional<P, Fail<P.Input, P.Output>> {
+    parser.map(Conditional.first) ?? .second(.init())
   }
 
   @inlinable
-  public static func buildLimitedAvailability<P>(_ component: P) -> Optionally<P>
-  where P: Parser {
-    .init { component }
+  public static func buildLimitedAvailability<P>(
+    _ parser: P?
+  ) -> Conditional<P, Fail<P.Input, P.Output>> {
+    parser.map(Conditional.first) ?? .second(.init())
   }
 }
--- Sources/Parsing/Builders/ParserBuilder.swift
+++ Sources/Parsing/Builders/ParserBuilder.swift
@@ -8,22 +8,14 @@ public enum ParserBuilder {
   @inlinable
   public static func buildEither<TrueParser, FalseParser>(
     first parser: TrueParser
-  ) -> Conditional<TrueParser, FalseParser>
-  where
-    TrueParser: Parser,
-    FalseParser: Parser
-  {
+  ) -> Conditional<TrueParser, FalseParser> {
     .first(parser)
   }
 
   @inlinable
   public static func buildEither<TrueParser, FalseParser>(
     second parser: FalseParser
-  ) -> Conditional<TrueParser, FalseParser>
-  where
-    TrueParser: Parser,
-    FalseParser: Parser
-  {
+  ) -> Conditional<TrueParser, FalseParser> {
     .second(parser)
   }
 
