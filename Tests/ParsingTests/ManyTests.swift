0a7c02eba575eeb8d90cc839692cc6da017a242d
Stephen Celis <stephen@stephencelis.com>
2022-01-14 12:17:16 -0500

    wip

--- Tests/ParsingTests/ManyTests.swift
+++ Tests/ParsingTests/ManyTests.swift
@@ -102,7 +102,12 @@ class ManyTests: XCTestCase {
   func testEmptyComponents() {
     var input = "2001:db8::2:1"[...]
     XCTAssertEqual(
-      Many(Prefix(while: \.isHexDigit), separator: ":").parse(&input),
+      Many {
+        Prefix(while: \.isHexDigit)
+      } separator: {
+        ":"
+      }
+      .parse(&input),
       ["2001", "db8", "", "2", "1"]
     )
   }
