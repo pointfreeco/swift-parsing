cd18bcab5da0ba026405a423a60b3a7a31d6feb3
Stephen Celis <stephen@stephencelis.com>
2022-01-14 13:34:17 -0500

    wip

--- Sources/Parsing/ParserPrinters/OneOfMany.swift
+++ Sources/Parsing/ParserPrinters/OneOfMany.swift
@@ -1,4 +1,22 @@
 extension Parsers {
+  /// A parser that attempts to run a number of parsers till one succeeds.
+  ///
+  /// You will not typically need to interact with this type directly. Instead you will usually loop
+  /// over each parser in a builder block:
+  ///
+  /// ```swift
+  /// enum Role: String, CaseIterable {
+  ///   case admin
+  ///   case guest
+  ///   case member
+  /// }
+  ///
+  /// Parse {
+  ///   for role in Role.allCases {
+  ///     status.rawValue.map { role }
+  ///   }
+  /// }
+  /// ```
   public struct OneOfMany<Parsers>: Parser where Parsers: Parser {
     public let parsers: [Parsers]
 
