c49901779e41cab84e5c0ede70a608eb842a4e9b
Stephen Celis <stephen@stephencelis.com>
2022-01-24 11:54:58 -0500

    wip

--- Sources/swift-parsing-benchmark/StringAbstractions.swift
+++ Sources/swift-parsing-benchmark/StringAbstractions.swift
@@ -4,20 +4,22 @@ import Parsing
 /*
  This benchmark demonstrates how to parse on multiple string abstractions at once, and the costs
  of doing so. The parsers benchmarked parse a list of integers that are separated by a
- UTF8 character with multiple equivalent representations: "é" and "é".
+ UTF8 character with multiple equivalent representations: "LATIN SMALL LETTER E WITH ACUTE" and
+ "E + COMBINING ACUTE ACCENT".
 
  In the "Substring" suite we parse integers on the UTF8View abstraction and parse the separator
  on the Substring abstraction in order to take advantage of its UTF8 normalization logic.
 
  In the "UTF8" suite we parse both the integers and the separators on the UTF8View abstraction,
  but this means we are responsible for handling UTF8 normalization, so we have to explicitly
- handle both the "é" and "é" characters.
+ handle both the "LATIN SMALL LETTER E WITH ACUTE" and "E + COMBINING ACUTE ACCENT" characters.
  */
+
 let stringAbstractionsSuite = BenchmarkSuite(name: "String Abstractions") { suite in
   let count = 1_000
   let input = (1...count)
     .reduce(into: "") { accum, int in
-      accum += "\(int)" + (int.isMultiple(of: 2) ? "é" : "é")
+      accum += "\(int)" + (int.isMultiple(of: 2) ? "\u{00E9}" : "e\u{0301}")
     }
     .dropLast()
 
@@ -26,7 +28,7 @@ let stringAbstractionsSuite = BenchmarkSuite(name: "String Abstractions") { suit
     let output = Many {
       Int.parser()
     } separator: {
-      FromSubstring { "é" }
+      FromSubstring { "\u{00E9}" }
     }
     .parse(&input)
     precondition(output?.count == count)
@@ -38,8 +40,8 @@ let stringAbstractionsSuite = BenchmarkSuite(name: "String Abstractions") { suit
       Int.parser()
     } separator: {
       OneOf {
-        "é".utf8
-        "é".utf8
+        "\u{00E9}".utf8
+        "e\u{0301}".utf8
       }
     }
     .parse(&input)
