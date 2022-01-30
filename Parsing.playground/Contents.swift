26a874e29ddc076f2dee6cfc82d337689bc05b86
Brandon Williams <mbrandonw@hey.com>
2022-01-30 12:50:54 -0800

    wip

--- Parsing.playground/Contents.swift
+++ Parsing.playground/Contents.swift
@@ -139,9 +139,137 @@ extension Prefix: Printer where Input == Substring {
   }
 }
 
-try Prefix(5)
-{ $0 != "," }
-.parse("Hello, World")
+try Prefix
+{ $0 != "," }.parse("Hello")
 
+//input = ""
+//try Prefix
+//{ $0 != "," }.print("Hello,", to: &input) // error was thrown: PrintingError()
+//input
+
+input = ""
+try Prefix
+{ $0 != "\"" }.print("Blob, Esq.", to: &input) // error was thrown: PrintingError()
+input
+
+/*
+ error: unexpected output
+
+ output:
+ "Hello,"
+       ^ element satisfying predicate
+ */
 
 1
+2
+
+extension Parse: Printer where Parsers: Printer {
+  func print(_ output: Parsers.Output, to input: inout Parsers.Input) throws {
+    try self.parsers.print(output, to: &input)
+  }
+}
+
+
+input = ""
+try Parse {
+  Prefix { $0 != "\"" }
+}
+.print("Blob, Esq.", to: &input)
+input
+
+
+extension Parsers.ZipVOV: Printer
+where
+  P0: Printer,
+  P1: Printer,
+  P2: Printer
+{
+  func print(_ output: (P1.Output), to input: inout P0.Input) throws {
+    try self.p0.print((), to: &input)
+    try self.p1.print(output, to: &input)
+    try self.p2.print((), to: &input)
+  }
+}
+
+//extension Parsers.Map: Printer where Upstream: Printer {
+//  func print(_ output: Output, to input: inout Upstream.Input) throws {
+//    self.upstream.print(<#T##output: Upstream.Output##Upstream.Output#>, to: &<#T##Upstream.Input#>)
+//  }
+//}
+
+
+let quotedField = Parse {
+  "\""
+  Prefix { $0 != "\"" }
+  "\""
+}
+
+input = ""
+try quotedField.print("Blob, Esq.", to: &input)
+input // ""Blob, Esq.""
+
+try quotedField.parse(&input)
+input // ""
+
+try quotedField.print("Blob, Esq.", to: &input)
+input // ""Blob, Esq.""
+
+extension OneOf: Printer where Parsers: Printer {
+  func print(_ output: Parsers.Output, to input: inout Parsers.Input) throws {
+    try self.parsers.print(output, to: &input)
+  }
+}
+
+extension Parsers.OneOf2: Printer where P0: Printer, P1: Printer {
+  func print(_ output: P0.Output, to input: inout P0.Input) throws {
+    do {
+      try self.p1.print(output, to: &input)
+    } catch {
+      try self.p0.print(output, to: &input)
+    }
+  }
+}
+
+
+let _field = OneOf {
+  Parse {
+    "\""
+    Prefix { $0 != "\"" }
+    "\""
+  }
+
+  Prefix { $0 != "," }
+}
+
+try field.parse("\"Blob, Esq.\",true") // "\"Blob,"
+
+
+input = ""
+try _field.print("Blob, Esq.", to: &input)
+input // ""Blob, Esq.""
+
+input = ""
+try _field.print("Blob Jr.", to: &input)
+input // ""Blob Jr.""
+
+
+extension Skip: Printer where Parsers: Printer, Parsers.Output == Void {
+  func print(_ output: (), to input: inout Parsers.Input) throws {
+    try self.parsers.print((), to: &input)
+  }
+}
+
+extension Parsers.ZipVV: Printer where P0: Printer, P1: Printer {
+  func print(_ output: (), to input: inout P0.Input) throws {
+    try self.p0.print((), to: &input)
+    try self.p1.print((), to: &input)
+  }
+}
+
+
+
+extension Parsers.IntParser: Printer where Input == Substring.UTF8View {
+  func print(_ output: Output, to input: inout Substring.UTF8View) throws {
+    input.append
+  }
+}
