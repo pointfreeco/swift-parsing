92ca193d343dffbff0492c61e658600ab7ed4b58
stephencelis <stephencelis@users.noreply.github.com>
2022-02-01 22:50:49 +0000

    Run swift-format

--- Tests/ParsingTests/CharacterSetTests.swift
+++ Tests/ParsingTests/CharacterSetTests.swift
@@ -2,45 +2,45 @@ import Parsing
 import XCTest
 
 final class CharacterSetTests: XCTestCase {
-    func testDecimalDigits() {
-        let characterSet = CharacterSet.decimalDigits
+  func testDecimalDigits() {
+    let characterSet = CharacterSet.decimalDigits
 
-        var input = "42abc"[...]
+    var input = "42abc"[...]
 
-        let output = characterSet.parse(&input)
-        XCTAssertEqual(output, "42")
-        XCTAssertEqual(input, "abc")
-    }
+    let output = characterSet.parse(&input)
+    XCTAssertEqual(output, "42")
+    XCTAssertEqual(input, "abc")
+  }
 
-    func testAlphanumerics() {
-        let characterSet = CharacterSet.alphanumerics
+  func testAlphanumerics() {
+    let characterSet = CharacterSet.alphanumerics
 
-        var input = "42abc;."[...]
+    var input = "42abc;."[...]
 
-        let output = characterSet.parse(&input)
-        XCTAssertEqual(output, "42abc")
-        XCTAssertEqual(input, ";.")
-    }
+    let output = characterSet.parse(&input)
+    XCTAssertEqual(output, "42abc")
+    XCTAssertEqual(input, ";.")
+  }
 
-    func testCustom() {
-        let characterSet = CharacterSet(charactersIn: "0123")
+  func testCustom() {
+    let characterSet = CharacterSet(charactersIn: "0123")
 
-        var input = "23456789abcdef"[...]
+    var input = "23456789abcdef"[...]
 
-        let output = characterSet.parse(&input)
-        XCTAssertEqual(output, "23")
-        XCTAssertEqual(input, "456789abcdef")
-    }
+    let output = characterSet.parse(&input)
+    XCTAssertEqual(output, "23")
+    XCTAssertEqual(input, "456789abcdef")
+  }
 
-    func testCompletion() throws {
-        let characterSet = CharacterSet(charactersIn: "123")
+  func testCompletion() throws {
+    let characterSet = CharacterSet(charactersIn: "123")
 
-        var input = "123456"[...]
+    var input = "123456"[...]
 
-        let firstOutput = try XCTUnwrap(characterSet.parse(&input))
-        let secondOutput = try XCTUnwrap(characterSet.parse(&input))
+    let firstOutput = try XCTUnwrap(characterSet.parse(&input))
+    let secondOutput = try XCTUnwrap(characterSet.parse(&input))
 
-        XCTAssertEqual(firstOutput, "123")
-        XCTAssertTrue(secondOutput.isEmpty)
-    }
+    XCTAssertEqual(firstOutput, "123")
+    XCTAssertTrue(secondOutput.isEmpty)
+  }
 }
