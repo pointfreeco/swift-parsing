d41a42c31149514a446cfa5c735ddd6bc0a1376e
Stephen Celis <stephen@stephencelis.com>
2022-02-04 12:47:19 -0500

    wip

--- Sources/_URLRouting/Printer.swift
+++ Sources/_URLRouting/Printer.swift
@@ -1,5 +1,4 @@
 extension Parser where Self: Printer, Input == URLRequestData {
-  @inlinable
   public func print(_ output: Output) rethrows -> URLRequestData {
     var request = URLRequestData()
     try self.print(output, to: &request)
