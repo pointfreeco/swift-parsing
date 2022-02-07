7701afc6e2280814868146b7f75db687710d47a8
Stephen Celis <stephen@stephencelis.com>
2022-02-07 14:00:27 -0500

    wip

---
+++ Sources/Parsing/Conversions/ConversionsParser.swift
@@ -0,0 +1,41 @@
+extension Conversion {
+  @inlinable
+  public static func parse<P>(
+    @ParserBuilder _ parser: () -> P
+  ) -> Self where Self == Conversions.Parser<P> {
+    .init(parser: parser())
+  }
+
+  @inlinable
+  public func parse<P>(
+    @ParserBuilder _ parser: () -> P
+  ) -> Conversions.Map<Self, Conversions.Parser<P>> {
+    self.map(.parse(parser))
+  }
+}
+
+extension Conversions {
+  public struct Parser<Upstream>: Conversion
+  where
+    Upstream: ParserPrinter,
+    Upstream.Input: AppendableCollection
+  {
+    public let parser: Upstream
+
+    @inlinable
+    public init(parser: Upstream) {
+      self.parser = parser
+    }
+
+    @inlinable
+    public func apply(_ input: Upstream.Input) rethrows -> Upstream.Output {
+      // TODO: Should `End` *not* be baked in?
+      try Parse { self.parser; End<Upstream.Input>() }.parse(input)
+    }
+
+    @inlinable
+    public func unapply(_ output: Upstream.Output) rethrows -> Upstream.Input {
+      try self.parser.print(output)
+    }
+  }
+}
