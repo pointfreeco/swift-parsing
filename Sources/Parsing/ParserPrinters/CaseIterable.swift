e4bdb0bca9503173c00198c92ab250314227ad84
Stephen Celis <stephen@stephencelis.com>
2022-02-09 10:44:21 -0500

    wip

--- Sources/Parsing/ParserPrinters/CaseIterable.swift
+++ Sources/Parsing/ParserPrinters/CaseIterable.swift
@@ -1,16 +1,19 @@
 extension CaseIterable where Self: RawRepresentable, RawValue == String {
+  @inlinable
   static func parser(
     of inputType: Substring.Type = Substring.self
   ) -> Parsers.CaseIterableRawRepresentableParser<Substring, Self> {
     .init(toInput: { $0[...] }, areEquivalent: ==)
   }
 
+  @inlinable
   static func parser(
     of inputType: Substring.UTF8View.Type = Substring.UTF8View.self
   ) -> Parsers.CaseIterableRawRepresentableParser<Substring.UTF8View, Self> {
     .init(toInput: { $0[...].utf8 }, areEquivalent: ==)
   }
 
+  @inlinable
   static func parser<Input>(
     of inputType: Input.Type = Input.self
   ) -> Parsers.CaseIterableRawRepresentableParser<Input, Self>
@@ -27,9 +30,22 @@ extension Parsers {
   public struct CaseIterableRawRepresentableParser<
     Input: Collection, Output: CaseIterable & RawRepresentable
   >: Parser where Input.SubSequence == Input {
+    @usableFromInline
     let toInput: (Output.RawValue) -> Input
+
+    @usableFromInline
     let areEquivalent: (Input.Element, Input.Element) -> Bool
 
+    @usableFromInline
+    init(
+      toInput: @escaping (Output.RawValue) -> Input,
+      areEquivalent: @escaping (Input.Element, Input.Element) -> Bool
+    ) {
+      self.toInput = toInput
+      self.areEquivalent = areEquivalent
+    }
+
+    @inlinable
     public func parse(_ input: inout Input) throws -> Output {
       for `case` in Output.allCases {
         let prefix = self.toInput(`case`.rawValue)
@@ -44,6 +60,7 @@ extension Parsers {
 }
 
 extension Parsers.CaseIterableRawRepresentableParser: Printer where Input: AppendableCollection {
+  @inlinable
   public func print(_ output: Output, to input: inout Input) {
     input.append(contentsOf: self.toInput(output.rawValue))
   }
