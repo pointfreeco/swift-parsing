1617aa7a42184262040bad1f51db5ec2836f12e7
Stephen Celis <stephen@stephencelis.com>
2022-02-08 19:09:31 -0500

    wip

---
+++ Sources/Parsing/Conversions/BinaryFloatingPoint.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+extension Conversion where Self == Conversions.FixedWidthIntegerToBinaryFloatingPoint<Int, Double> {
+  @inlinable
+  public static var double: Self { .init() }
+}
+
+extension Conversion where Output == Int {
+  @inlinable
+  public var double: Conversions.Map<
+    Self, Conversions.FixedWidthIntegerToBinaryFloatingPoint<Int, Double>
+  > { self.map(.double) }
+}
+
+extension Conversions {
+  public struct FixedWidthIntegerToBinaryFloatingPoint<
+    Input: FixedWidthInteger, Output: BinaryFloatingPoint
+  >: Conversion {
+    @usableFromInline
+    init () {}
+
+    @inlinable
+    public func apply(_ input: Input) -> Output {
+      .init(input)
+    }
+
+    @inlinable
+    public func unapply(_ output: Output) -> Input {
+      .init(output)
+    }
+  }
+}
---
+++ Sources/Parsing/Conversions/FixedWidthInteger.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+extension Conversion where Self == Conversions.BinaryFloatingPointToFixedWidthInteger<Double, Int> {
+  @inlinable
+  public static var int: Self { .init() }
+}
+
+extension Conversion where Output == Double {
+  @inlinable
+  public var double: Conversions.Map<
+    Self, Conversions.BinaryFloatingPointToFixedWidthInteger<Double, Int>
+  > { self.map(.int) }
+}
+
+
+extension Conversions {
+  public struct BinaryFloatingPointToFixedWidthInteger<
+    Input: BinaryFloatingPoint, Output: FixedWidthInteger
+  >: Conversion {
+    @usableFromInline
+    init () {}
+
+    @inlinable
+    public func apply(_ input: Input) -> Output {
+      .init(input)
+    }
+
+    @inlinable
+    public func unapply(_ output: Output) -> Input {
+      .init(output)
+    }
+  }
+}
