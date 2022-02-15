8230071a038feb383ff269699e64a2c38f95fca3
Stephen Celis <stephen@stephencelis.com>
2022-02-15 15:32:32 -0500

    wip

--- Sources/Parsing/Conversions/ParseableFormatStyle.swift
+++ Sources/Parsing/Conversions/ParseableFormatStyle.swift
@@ -2,12 +2,27 @@ import Foundation
 
 @available(iOS 15, macOS 12, tvOS 15, watchOS 8, *)
 extension Conversion {
+  /// A conversion that wraps a parseable format style.
+  ///
+  /// See ``formatted(_:)-swift.method`` for a fluent version of this interface that transforms an
+  /// existing conversion.
+  ///
+  /// - Parameter style: A parseable format style.
+  /// - Returns: A conversion from a string to the given type.
   public static func formatted<Style>(
     _ style: Style
   ) -> Self where Self == Conversions.ParseableFormat<Style> {
     .init(style: style)
   }
 
+  /// Transforms this conversion to a parseable format style's parse input into a conversion to the
+  /// parseable format style's parse output.
+  ///
+  /// A fluent version of ``formatted(_:)-swift.type.method``. Equivalent to calling
+  /// `Conversion.map(.formatted(style)).
+  ///
+  /// - Parameter type: A type that conforms to `LosslessStringConvertible`.
+  /// - Returns: A conversion from a string to the given type.
   public func formatted<Style>(
     _ style: Style
   ) -> Conversions.Map<Self, Conversions.ParseableFormat<Style>> {
@@ -16,12 +31,18 @@ extension Conversion {
 }
 
 extension Conversions {
+  /// A conversion for a parseable format style.
+  ///
+  /// You will not typically need to interact with this type directly. Instead you will usually use
+  /// the ``Conversion/formatted(_:)-swift.type.method`` and
+  /// ``Conversion/formatted(_:)-swift.method`` operations, which construct this type under the
+  /// hood.
   @available(iOS 15, macOS 12, tvOS 15, watchOS 8, *)
   public struct ParseableFormat<Style>: Conversion where Style: ParseableFormatStyle {
     public let style: Style
 
     @inlinable
-    public func apply(_ input: Style.FormatOutput) throws -> Style.FormatInput {
+    public func apply(_ input: Style.Strategy.ParseInput) throws -> Style.Strategy.ParseOutput {
       try style.parseStrategy.parse(input)
     }
 
