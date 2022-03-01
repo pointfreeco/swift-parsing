fb2f7c4d0e70698fb805cd7c0f884edc58df3740
David Peterson <david@randombits.org>
2022-03-02 07:37:13 +1000

    Added backtracking to Optionally (#149)
    * Added backtracking to Optionally
    
    Also updated documentation about backtracking in general.
    
    * update docs
    
    Co-authored-by: Brandon Williams <mbrandonw@hey.com>

--- Sources/Parsing/Parsers/Optionally.swift
+++ Sources/Parsing/Parsers/Optionally.swift
@@ -2,6 +2,24 @@
 ///
 /// Use this parser when you are parsing into an output data model that contains `nil`.
 ///
+/// When the wrapped parser fails ``Optionally`` will backtrack any consumption of the input so
+/// that later parsers can attempt to parser the input:
+///
+/// ```swift
+/// let parser = Parse {
+///   "Hello,"
+///   Optionally { " "; Bool.parser() }
+///   " world!"
+/// }
+///
+/// try parser.parse("Hello, world!")      // nil 1️⃣
+/// try parser.parse("Hello, true world!") // true
+/// ```
+///
+/// If ``Optionally`` did not backtrack then 1️⃣ would fail because it would consume a space,
+/// causing the `" world!"` parser to fail since there is no longer any space to consume.
+/// Read the article <doc:Backtracking> to learn more about how backtracking works.
+///
 /// If you are optionally parsing input that should coalesce into some default, you can skip the
 /// optionality and instead use ``replaceError(with:)`` with a default:
 ///
@@ -24,6 +42,12 @@ public struct Optionally<Wrapped: Parser>: Parser {
 
   @inlinable
   public func parse(_ input: inout Wrapped.Input) -> Wrapped.Output? {
-    try? self.wrapped.parse(&input)
+    let original = input
+    do {
+     return try self.wrapped.parse(&input)
+    } catch {
+     input = original
+     return nil
+    }
   }
 }
