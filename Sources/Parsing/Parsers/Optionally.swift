679754a8d08639dff3d7a093b99bddb2838e17f7
Brandon Williams <mbrandonw@hey.com>
2022-03-01 13:34:23 -0800

    update docs

--- Sources/Parsing/Parsers/Optionally.swift
+++ Sources/Parsing/Parsers/Optionally.swift
@@ -1,6 +1,24 @@
-/// A parser that runs the given parser and succeeds with `nil` if it fails, backtracking the `input` in that case.
+/// A parser that runs the given parser and succeeds with `nil` if it fails.
 ///
 /// Use this parser when you are parsing into an output data model that contains `nil`.
+///
+/// When the wrapped parser fails ``Optionally`` will backtrack any consumption of the input so
+/// that later parsers can attempt to parser the input:
+///
+/// ```swift
+/// let parser = Parse {
+///   "Hello,"
+///   Optionally { " "; Bool.parser() }
+///   " world!"
+/// }
+///
+/// try parser.parse("Hello, world!")      // nil 1️⃣
+/// try parser.parse("Hello, true world!") // true
+/// ```
+///
+/// If ``Optionally`` did not backtrack then 1️⃣ would fail because it would consume a space,
+/// causing the `" world!"` parser to fail since there is no longer any space to consume.
+/// Read the article <doc:Backtracking> to learn more about how backtracking works.
 ///
 /// If you are optionally parsing input that should coalesce into some default, you can skip the
 /// optionality and instead use ``replaceError(with:)`` with a default:
