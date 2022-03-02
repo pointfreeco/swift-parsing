a0c4ee657c2b1433293bf1834cbd9dbedc1347c4
Stephen Celis <stephen@stephencelis.com>
2022-03-02 10:52:13 -0500

    wip

--- Tests/ParsingTests/PeekTests.swift
+++ Tests/ParsingTests/PeekTests.swift
@@ -58,70 +58,13 @@ class PeekTests: XCTestCase {
     }
     XCTAssertNoDifference("blah"[...], input)
   }
-  
-//  func testPrintSucceeds() {
-//    var input = "alpha"[...]
-//    
-//    let parser = Peek { Prefix(1) { $0.isLetter } }
-//    
-//    XCTAssertNoThrow(try parser.print("a", to: &input))
-//    XCTAssertNoDifference(input, "alpha"[...])
-//  }
-//  
-//  func testPrintFailsWithIncorrectOutput() {
-//    var input = "alpha"[...]
-//    
-//    let parser = Peek { Prefix(1) { $0.isLetter } }
-//    
-//    XCTAssertThrowsError(try parser.print("b", to: &input))
-//    XCTAssertNoDifference(input, "alpha"[...])
-//  }
-//  
-//  func testPrintFailsWithIncorrectInput() {
-//    var input = "1alpha"[...]
-//    
-//    let parser = Peek { Prefix(1) { $0.isLetter } }
-//    
-//    XCTAssertThrowsError(try parser.print("1", to: &input))
-//    XCTAssertNoDifference(input, "1alpha"[...])
-//  }
-//  
-//  func testPrintCompositePeekSucceeds() {
-//    var input = "!"[...]
-//
-//    let identifier = Parse {
-//      Peek {
-//        Prefix(1) { $0.isLetter || $0 == "_" }
-//      }
-//      Prefix { $0.isNumber || $0.isLetter || $0 == "_" }
-//    }
-//
-//    XCTAssertNoThrow(try identifier.print(("f", "foo_1"[...]), to: &input))
-//    XCTAssertNoDifference(input, "foo_1!")
-//  }
-//  
-//  func testPrintCompositePeekFails() {
-//    var input = "!"[...]
-//
-//    let identifier = Parse {
-//      Peek {
-//        Prefix(1) { $0.isLetter || $0 == "_" }
-//      }
-//      Prefix { $0.isNumber || $0.isLetter || $0 == "_" }
-//    }
-//
-//    XCTAssertThrowsError(try identifier.print(("1", "1foo_1"[...]), to: &input))
-//    XCTAssertNoDifference(input, "1foo_1!")
-//  }
-  
+
   func testPrintSkippedPeekSucceeds() {
     var input = "!"[...]
 
     let identifier = Parse {
-      Skip {
-        Peek {
-          Prefix(1) { $0.isLetter || $0 == "_" }
-        }
+      Peek {
+        Prefix(1) { $0.isLetter || $0 == "_" }
       }
       .printing("") // this will bypass actually printing the upstream parser in `Skip`
       Prefix { $0.isNumber || $0.isLetter || $0 == "_" }
@@ -135,10 +78,8 @@ class PeekTests: XCTestCase {
     var input = "!"[...]
 
     let identifier = Parse {
-      Skip {
-        Peek {
-          Prefix(1) { $0.isLetter || $0 == "_" }
-        }
+      Peek {
+        Prefix(1) { $0.isLetter || $0 == "_" }
       }
       .printing("") // this will bypass actually printing the upstream parser in `Skip`
       Prefix { $0.isNumber || $0.isLetter || $0 == "_" }
@@ -148,5 +89,49 @@ class PeekTests: XCTestCase {
     // but parses because of `.printing("")` statement bypasses the `Peek`.
     XCTAssertNoThrow(try identifier.print("1foo"[...], to: &input))
     XCTAssertNoDifference(input, "1foo!")
+  }
+
+  func testPrintUpstreamParses() {
+    var input = "// a comment"[...]
+    let parser = Peek { "//" }
+    XCTAssertNoThrow(try parser.print((), to: &input))
+    XCTAssertNoDifference(input, "// a comment"[...])
+  }
+
+  func testPrintUpstreamFails() {
+    var input = "not a comment"[...]
+    let parser = Peek { "//" }
+    XCTAssertThrowsError(try parser.print((), to: &input))
+    XCTAssertNoDifference(input, "not a comment"[...])
+  }
+
+  func testPrintComplexParserSucceeds() {
+    var input = ""[...]
+
+    let commentedLine = Parse {
+      Peek { "//" }
+      Rest()
+    }
+
+    XCTAssertNoThrow(try commentedLine.print("// commented line"[...], to: &input))
+    XCTAssertNoDifference(
+      input,
+      "// commented line"
+    )
+  }
+
+  func testPrintComplexParserFails() {
+    var input = ""[...]
+
+    let commentedLine = Parse {
+      Peek { "//" }
+      Rest()
+    }
+
+    XCTAssertThrowsError(try commentedLine.print("uncommented line"[...], to: &input))
+    XCTAssertNoDifference(
+      input,
+      "uncommented line"
+    )
   }
 }
