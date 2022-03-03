532629f32f278df6d76567a21fc1c0098c189130
Stephen Celis <stephen@stephencelis.com>
2022-03-03 16:25:54 -0500

    wip

--- Tests/ParsingTests/OptionallyTests.swift
+++ Tests/ParsingTests/OptionallyTests.swift
@@ -14,7 +14,7 @@ final class OptionalTests: XCTestCase {
     XCTAssertNoDifference(.none, Optionally { Bool.parser() }.parse(&input))
     XCTAssertNoDifference("Hello, world!", Substring(input))
   }
-    
+
   func testParseBacktracking() {
     let parser = Parse {
       "Hello,"
@@ -28,25 +28,26 @@ final class OptionalTests: XCTestCase {
     XCTAssertNoDifference(.some(true), try parser.parse("Hello, true world!"))
     XCTAssertNoDifference(.none, try parser.parse("Hello, world!"))
   }
-  
+
   func testPrintNilSuccess() {
     var input = "!"[...]
-    
+
     XCTAssertNoThrow(try Optionally { Bool.parser() }.print(.none, into: &input))
     XCTAssertNoDifference("!"[...], input)
   }
-  
+
   func testPrintValueSuccess() {
     var input = "!"[...]
-    
+
     XCTAssertNoThrow(try Optionally { Bool.parser() }.print(.some(true), into: &input))
     XCTAssertNoDifference("true!"[...], input)
   }
-  
+
   func testPrintBadOptionalValue() {
     var input = "!"[...]
-    
-    XCTAssertThrowsError(try Optionally { Prefix { !$0.isWhitespace } }.print("foo bar", into: &input))
+
+    XCTAssertThrowsError(
+      try Optionally { Prefix { !$0.isWhitespace } }.print("foo bar", into: &input))
     XCTAssertNoDifference("!"[...], input)
   }
 }
