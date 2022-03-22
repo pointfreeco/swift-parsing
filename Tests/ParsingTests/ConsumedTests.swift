2d203887a034050c1072d75667e267a4b724a7be
Stephen Celis <stephen@stephencelis.com>
2022-03-22 11:08:09 -0400

    Consumed parser (#187)
    A parser that returns the underlying sequence that was parsed.

---
+++ Tests/ParsingTests/ConsumedTests.swift
@@ -0,0 +1,10 @@
+import Parsing
+import XCTest
+
+final class ConsumedTests: XCTestCase {
+  func testConsumed() throws {
+    var input = "    \r \t\t \r\n \n\r    Hello, world!"[...].utf8
+    XCTAssertEqual("    \r \t\t \r\n \n\r    ", Substring(Consumed { Whitespace() }.parse(&input)))
+    XCTAssertEqual("Hello, world!", Substring(input))
+  }
+}
