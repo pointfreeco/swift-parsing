12a8cfb70dda1eebb9f4b7cfb9705f3f70e6565a
Stephen Celis <stephen@stephencelis.com>
2022-03-22 16:54:53 -0400

    wip

--- Tests/ParsingTests/ConsumedTests.swift
+++ Tests/ParsingTests/ConsumedTests.swift
@@ -4,7 +4,10 @@ import XCTest
 final class ConsumedTests: XCTestCase {
   func testConsumed() throws {
     var input = "    \r \t\t \r\n \n\r    Hello, world!"[...].utf8
-    XCTAssertEqual("    \r \t\t \r\n \n\r    ", Substring(Consumed { Whitespace() }.parse(&input)))
+    XCTAssertEqual(
+      "    \r \t\t \r\n \n\r    ",
+      try Substring(Consumed { Whitespace() }.parse(&input))
+    )
     XCTAssertEqual("Hello, world!", Substring(input))
   }
 }
--- Tests/ParsingTests/WhitespaceTests.swift
+++ Tests/ParsingTests/WhitespaceTests.swift
@@ -4,25 +4,25 @@ import XCTest
 final class WhitespaceTests: XCTestCase {
   func testTrimsAllWhitespace() {
     var input = "    \r \t\t \r\n \n\r    Hello, world!"[...].utf8
-    XCTAssertNotNil(Whitespace().parse(&input))
+    XCTAssertNotNil(try Whitespace().parse(&input))
     XCTAssertEqual("Hello, world!", Substring(input))
   }
 
   func testAlwaysSucceeds() {
     var input = "Hello, world!"[...].utf8
-    XCTAssertNotNil(Whitespace().parse(&input))
+    XCTAssertNotNil(try Whitespace().parse(&input))
     XCTAssertEqual("Hello, world!", Substring(input))
   }
 
   func testTrimsHorizontalWhitespace() {
     var input = "    \r \t\t \r\n \n\r    Hello, world!"[...].utf8
-    XCTAssertNotNil(Whitespace(.horizontal).parse(&input))
+    XCTAssertNotNil(try Whitespace(.horizontal).parse(&input))
     XCTAssertEqual("\r \t\t \r\n \n\r    Hello, world!", Substring(input))
   }
 
   func testTrimsVerticalWhitespace() {
     var input = "\r\n\r\n \n\r    Hello, world!"[...].utf8
-    XCTAssertNotNil(Whitespace(.vertical).parse(&input))
+    XCTAssertNotNil(try Whitespace(.vertical).parse(&input))
     XCTAssertEqual(" \n\r    Hello, world!", Substring(input))
   }
 }
