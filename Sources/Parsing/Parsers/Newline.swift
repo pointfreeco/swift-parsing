d80f3b7a23637448829ef0c8291dc6306be79e82
Stephen Celis <stephen@stephencelis.com>
2022-03-28 12:26:55 -0400

    wip

--- Sources/Parsing/Parsers/Newline.swift
+++ Sources/Parsing/Parsers/Newline.swift
@@ -9,51 +9,27 @@
 ///   Prefix(1) { $0.isNewline }
 ///   ```
 /// It will consume both line feeds (`"\n"`) and carriage returns with line feeds (`"\r\n"`).
-public struct Newline<Input: Collection, Bytes: Collection>: Parser
+public struct Newline<Input: Collection>: Parser
 where
   Input.SubSequence == Input,
-  Bytes.SubSequence == Bytes,
-  Bytes.Element == UTF8.CodeUnit
+  Input.Element == UTF8.CodeUnit
 {
-  @usableFromInline
-  let toBytes: (Input) -> Bytes
-
-  @usableFromInline
-  let fromBytes: (Bytes) -> Input
+  @inlinable
+  public init() {}
 
   @inlinable
   public func parse(_ input: inout Input) throws {
-    var bytes = self.toBytes(input)
-    if bytes.first == .init(ascii: "\n") {
-      bytes.removeFirst()
-    } else if bytes.first == .init(ascii: "\r"), bytes.dropFirst().first == .init(ascii: "\n") {
-      bytes.removeFirst(2)
+    if input.first == .init(ascii: "\n") {
+      input.removeFirst()
+    } else if input.first == .init(ascii: "\r"), input.dropFirst().first == .init(ascii: "\n") {
+      input.removeFirst(2)
     } else {
       throw ParsingError.expectedInput(#""\n" or "\r\n""#, at: input)
     }
-    input = self.fromBytes(bytes)
   }
 }
 
-// NB: Swift 5.5.2 on Linux and Windows fails to build with a simpler `Bytes == Input` constraint
-extension Newline where Bytes == Input.SubSequence, Bytes.SubSequence == Input {
-  @inlinable
-  public init() {
-    self.toBytes = { $0 }
-    self.fromBytes = { $0 }
-  }
-}
-
-extension Newline where Input == Substring, Bytes == Substring.UTF8View {
-  @_disfavoredOverload
-  @inlinable
-  public init() {
-    self.toBytes = { $0.utf8 }
-    self.fromBytes = Substring.init
-  }
-}
-
-extension Newline where Input == Substring.UTF8View, Bytes == Input {
+extension Newline where Input == Substring.UTF8View {
   @_disfavoredOverload
   @inlinable
   public init() { self.init() }
