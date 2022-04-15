671ca208e711f7df02308d9183456721ac982065
Stephen Celis <stephen@stephencelis.com>
2022-04-14 21:06:12 -0400

    wip

--- Sources/_URLRouting/PathEnd.swift
+++ Sources/_URLRouting/PathEnd.swift
@@ -4,11 +4,13 @@ public struct PathEnd: ParserPrinter {
 
   @inlinable
   public func parse(_ input: inout URLRequestData) throws {
-    try input.path.first.map { try End().parse($0) } ?? End().parse(input.path)
+    guard var first = input.path.first else { return }
+    try End().parse(&first)
   }
 
   @inlinable
   public func print(_ output: (), into input: inout Input) throws {
-    try End().print((), into: &input.path)
+    guard var first = input.path.first else { return }
+    try End().print((), into: &first)
   }
 }
