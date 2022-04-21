7c30b1bd28b49ed97408be499c6476d8599a2160
Point-Free <point-free@Brandons-MacBook-Pro.local>
2022-04-21 09:13:25 -0700

    wip

--- Parsing.playground/Contents.swift
+++ Parsing.playground/Contents.swift
@@ -1,89 +1,30 @@
-import Parsing
+
 
 /*
- This playground builds a parser for [day 13][AoC13] of the Advent of Code 2021 challenge.
 
- [AoC13]: https://adventofcode.com/2021/day/13
+Rails.application.routes.draw do
+  get "/users/:user_id/books/:book_id" => "books#fetch"
+end
+
+ users_books_path true, "hello" # /users/42/books/123
+
+
+app.get('/users/:userId/books/:bookId', function (req, res) {
+  ...
+})
+
+
+app.get("users", ":userID", "books", ":bookID") { req in
+  ...
+}
+
+
+<a href="/users/42/book/123">Blob Autobiography</a>
+<a href="/users/42/book/321">Blobbed around the world</a>
+
+ "/users/\(user.id)/books/\(book.id)"
+
+ router.parse("/users/42/books/123") // (42, 123)
+ router.print((user.id, book.id)) // "/users/42/books/123"
+
  */
-
-/// Sample input to be parsed.
-let input = """
-  6,10
-  0,14
-  9,10
-  0,3
-  10,4
-  4,11
-  6,0
-  6,12
-  4,1
-  0,13
-  10,12
-  3,4
-  3,0
-  8,4
-  1,10
-  2,14
-  8,10
-  9,0
-
-  fold along y=7
-  fold along x=5
-  """
-
-// MARK: - Models
-
-struct Dot {
-  let x, y: Int
-}
-
-enum Direction: String, CaseIterable {
-  case x, y
-}
-
-struct Fold {
-  let direction: Direction
-  let position: Int
-}
-
-struct Instructions {
-  let dots: [Dot]
-  let folds: [Fold]
-}
-
-// MARK: - Parsers
-
-let dot = ParsePrint(.memberwise(Dot.init)) {
-  Digits()
-  ","
-  Digits()
-}
-
-let fold = ParsePrint(.memberwise(Fold.init)) {
-  "fold along "
-  Direction.parser()
-  "="
-  Digits()
-}
-
-let instructions = ParsePrint(.memberwise(Instructions.init)) {
-  Many {
-    dot
-  } separator: {
-    "\n"
-  } terminator: {
-    "\n\n"
-  }
-  Many {
-    fold
-  } separator: {
-    "\n"
-  }
-}
-
-// MARK: Round-trip
-
-let parsed = try instructions.parse(input)
-let printed = try instructions.print(parsed)
-
-input == printed
