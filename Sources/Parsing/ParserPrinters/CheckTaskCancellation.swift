d01305f2ee6f8e08b041ce15530a9abfcc730f07
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2022-06-10 01:49:34 -0300

    Add CheckTaskCancellation

---
+++ Sources/Parsing/ParserPrinters/CheckTaskCancellation.swift
@@ -0,0 +1,54 @@
+/// A trivial parser that fails if the parent task is cancelled.
+///
+/// Parsing can be a long-running procedure. For this reason, you may want to abord the execution of
+/// a parser if its parent task is cancelled. When you compose parsers using a ``ParserBuilder``,
+/// you can insert this trivial parser before a potentially time-consuming sequence of parsers to
+/// abord processing it in any case the parent task is cancelled, and free the current thread. For
+/// example:
+///
+/// ```swift
+/// let parser = Parser {
+///   Many {
+///     CheckTaskCancellation()
+///     csvLineParser
+///   }
+/// }
+/// ```
+///
+/// This parser's sole purpose is to stop processing if it is evaluated when the parent task is
+/// cancelled. It doesn't affect parsing or printing results.
+///
+/// - Throws: This parser throws a `CancellationError` if the parent task is cancelled.
+public struct CheckTaskCancellation<Input>: ParserPrinter {
+  /// Construct a parser that runs the next parsers only if the parent task is not cancelled.
+  @inlinable
+  public init() {}
+
+  @inlinable
+  func checkTaskCancellationIfNeeded() throws {
+    if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+      #if canImport(_Concurrency) && compiler(>=5.5.2)
+      try Task.checkCancellation()
+      #endif
+    }
+  }
+
+  @inlinable
+  public func parse(_ input: inout Input) throws {
+    try checkTaskCancellationIfNeeded()
+  }
+  @inlinable
+  public func print(_ output: Void, into input: inout Input) throws {
+    try checkTaskCancellationIfNeeded()
+  }
+}
+
+extension CheckTaskCancellation where Input == Substring {
+  @inlinable
+  public init() {}
+}
+
+extension CheckTaskCancellation where Input == Substring.UTF8View {
+  @inlinable
+  public init() {}
+}
---
+++ Tests/ParsingTests/CheckTaskCancellationTests.swift
@@ -0,0 +1,31 @@
+import Parsing
+import XCTest
+  
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *)
+final class CheckTaskCancellationTests: XCTestCase {
+  #if canImport(_Concurrency) && compiler(>=5.5.2)
+  func testCheckClassCancellation() async throws {
+    let parser = Parse {
+      Many {
+        CheckTaskCancellation()
+        Digits(1)
+      } separator: {
+        ","
+      }
+    }
+    let count = 1_000_000
+    
+    let task = Task {
+      var values = Array(repeating: "1", count: count).joined(separator: ",")[...]
+      return try parser.parse(&values)
+    }
+    // We let the task run for a little while before cancelling it:
+    try await Task.sleep(nanoseconds: NSEC_PER_SEC / 10)
+    task.cancel()
+    // The parser shouldn't have had the time to parse all the digits, and should have stopped
+    // where it was was when the task was cancelled:
+    let result = try await task.value
+    XCTAssert(result.count < count)
+  }
+  #endif
+}
