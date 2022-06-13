2f339b699a03045bf0c3560a636ce2fe6ec804bf
Jaap Wijnen <wijnen.jaap@gmail.com>
2022-06-13 19:23:03 +0200

    add PipeEndTests.swift

---
+++ Tests/ParsingTests/PipeEndTests.swift
@@ -0,0 +1,66 @@
+@testable import Parsing
+import XCTest
+
+final class PipeEndTests: XCTestCase {
+  func testSuccess() {
+    var input = ""[...]
+      XCTAssertNoThrow(try Parsers.PipeEnd().parse(&input))
+    XCTAssertEqual("", input)
+  }
+
+  func testFailure() {
+    var input = "Hello, world!"[...]
+      XCTAssertThrowsError(try Parsers.PipeEnd().parse(&input)) { error in
+      XCTAssertEqual(
+        """
+        error: unexpected input
+         --> input:1:1-13
+        1 | Hello, world!
+          | ^^^^^^^^^^^^^ expected end of pipe
+        """,
+        "\(error)"
+      )
+    }
+    XCTAssertEqual("Hello, world!", input)
+  }
+
+  func testPrintSuccess() {
+    var input = ""[...]
+      XCTAssertNoThrow(try Parsers.PipeEnd().print(into: &input))
+    XCTAssertEqual(input, ""[...])
+  }
+
+  func testPrintFailure() {
+    var input = "Hello, world!"[...]
+      XCTAssertThrowsError(try Parsers.PipeEnd().print(into: &input)) { error in
+      XCTAssertEqual(
+        """
+        error: round-trip expectation failed
+
+        A "PipeEnd" parser-printer expected no more input, but more was printed.
+
+        "Hello, world!"
+
+        During a round-trip, the "PipeEnd" parser-printer would have failed to parse at this \
+        remaining input.
+        """,
+        "\(error)"
+      )
+    }
+    XCTAssertEqual(input, "Hello, world!"[...])
+  }
+
+  func testTrailingWhitespace() {
+    XCTAssertThrowsError(try Int.parser().parse("123   ")) { error in
+      XCTAssertEqual(
+        """
+        error: unexpected input
+         --> input:1:4
+        1 | 123␣␣␣
+          |    ^ expected end of input
+        """,
+        "\(error)"
+      )
+    }
+  }
+}
