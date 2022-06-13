b311ec18fb510511110a4b33eacd63e9158f0ca8
Jaap Wijnen <JaapWijnen@users.noreply.github.com>
2022-06-13 23:34:35 +0200

    Make PipeEnd a ParserPrinter (#227)
    * make PipeEnd ParserPrinter
    
    * add PipeEndTests.swift
    
    * remove End parser related test from PipeEndTests
    
    * Apply suggestions from code review
    
    Co-authored-by: Stephen Celis <stephen.celis@gmail.com>

---
+++ Tests/ParsingTests/PipeEndTests.swift
@@ -0,0 +1,52 @@
+@testable import Parsing
+import XCTest
+
+final class PipeEndTests: XCTestCase {
+  func testSuccess() {
+    var input = ""[...]
+      XCTAssertNoThrow(try Parsers.PipeEnd().parse(&input))
+    XCTAssertEqual("", input)
+  }
+
+  func testFailure() {
+    var input = "Hello, world!"[...]
+      XCTAssertThrowsError(try Parsers.PipeEnd().parse(&input)) { error in
+      XCTAssertEqual(
+        """
+        error: unexpected input
+         --> input:1:1-13
+        1 | Hello, world!
+          | ^^^^^^^^^^^^^ expected end of pipe
+        """,
+        "\(error)"
+      )
+    }
+    XCTAssertEqual("Hello, world!", input)
+  }
+
+  func testPrintSuccess() {
+    var input = ""[...]
+      XCTAssertNoThrow(try Parsers.PipeEnd().print(into: &input))
+    XCTAssertEqual(input, ""[...])
+  }
+
+  func testPrintFailure() {
+    var input = "Hello, world!"[...]
+      XCTAssertThrowsError(try Parsers.PipeEnd().print(into: &input)) { error in
+      XCTAssertEqual(
+        """
+        error: round-trip expectation failed
+
+        A piped parser-printer expected no more input, but more was printed.
+
+        "Hello, world!"
+
+        During a round-trip, the piped parser-printer would have failed to parse at this \
+        remaining input.
+        """,
+        "\(error)"
+      )
+    }
+    XCTAssertEqual(input, "Hello, world!"[...])
+  }
+}
