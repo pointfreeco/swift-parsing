bc92e84968990b41640214b636667f35b6e5d44c
Stephen Celis <stephen@stephencelis.com>
2022-06-28 18:29:56 -0400

    Add minimum platform requirements (#237)

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 
 let package = Package(
   name: "swift-parsing",
+  platforms: [
+    .iOS(.v13),
+    .macOS(.v10_15),
+    .tvOS(.v13),
+    .watchOS(.v6),
+  ],
   products: [
     .library(
       name: "Parsing",
