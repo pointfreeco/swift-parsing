6d9eea0dbfde9911fb7284c310be5005fa24cc49
Kai Oelfke <k.oelfke@me.com>
2023-03-27 15:05:23 +0800

    FromSubstring ParseBuilder needs Substring as Input

--- Tests/ParsingTests/FromSubstringTests.swift
+++ Tests/ParsingTests/FromSubstringTests.swift
@@ -31,4 +31,34 @@ final class FromSubstringTests: XCTestCase {
     XCTAssertNoThrow(try p.parse(&input))
     XCTAssert(input.isEmpty)
   }
+
+  func testDeprecatedUTF8View() {
+    let p = Parse(input: Substring.UTF8View.self) {
+      "caf".utf8
+      FromSubstring { "é" }
+    }
+
+    var input = "caf\u{00E9}"[...].utf8
+    XCTAssertNoThrow(try p.parse(&input))
+    XCTAssert(input.isEmpty)
+
+    input = "cafe\u{0301}"[...].utf8
+    XCTAssertNoThrow(try p.parse(&input))
+    XCTAssert(input.isEmpty)
+  }
+
+  func testDeprecatedUnicodeScalarView() {
+    let p = Parse {
+      "caf".unicodeScalars
+      FromSubstring { "é" }
+    }
+
+    var input = "caf\u{00E9}"[...].unicodeScalars
+    XCTAssertNoThrow(try p.parse(&input))
+    XCTAssert(input.isEmpty)
+
+    input = "cafe\u{0301}"[...].unicodeScalars
+    XCTAssertNoThrow(try p.parse(&input))
+    XCTAssert(input.isEmpty)
+  }
 }
