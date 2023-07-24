2cd318ee0bbb711965dc1e0be1e4eb612158327a
Stephen Celis <stephen@stephencelis.com>
2023-07-24 11:00:42 -0700

    wip

--- Sources/swift-parsing-benchmark/Date.swift
+++ Sources/swift-parsing-benchmark/Date.swift
@@ -2,73 +2,75 @@ import Benchmark
 import Foundation
 import Parsing
 
-struct DateTime: Parser {
-  var body: some Parser<Substring.UTF8View, Date> {
-    Parse(Date.init(year:month:day:hour:minute:second:nanosecond:timeZone:)) {
-      Digits(4)
-      "-".utf8
-      Digits(2).filter { (1...12).contains($0) }
-      "-".utf8
-      Digits(2).filter { (1...31).contains($0) }
-      "T".utf8
-      Digits(2).filter { $0 < 24 }
-      ":".utf8
-      Digits(2).filter { $0 < 60 }
-      ":".utf8
-      Digits(2).filter { $0 <= 60 }
-      Parse {
-        ".".utf8
-        Prefix(1...9, while: (UInt8(ascii: "0")...UInt8(ascii: "9")).contains)
-          .compactMap { n in Int(Substring(n)).map { $0 * Int(pow(10, 9 - Double(n.count))) } }
-      }
-      .replaceError(with: 0)
-      OneOf {
-        "Z".utf8.map { 0 }
+#if swift(>=5.8)
+  struct DateTime: Parser {
+    var body: some Parser<Substring.UTF8View, Date> {
+      Parse(Date.init(year:month:day:hour:minute:second:nanosecond:timeZone:)) {
+        Digits(4)
+        "-".utf8
+        Digits(2).filter { (1...12).contains($0) }
+        "-".utf8
+        Digits(2).filter { (1...31).contains($0) }
+        "T".utf8
+        Digits(2).filter { $0 < 24 }
+        ":".utf8
+        Digits(2).filter { $0 < 60 }
+        ":".utf8
+        Digits(2).filter { $0 <= 60 }
         Parse {
-          OneOf {
-            "+".utf8.map { 1 }
-            "-".utf8.map { -1 }
-          }
-          Digits(2).filter { $0 < 24 }.map { $0 * 60 * 60 }
-          ":".utf8
-          Digits(2).filter { $0 < 60 }.map { $0 * 60 }
+          ".".utf8
+          Prefix(1...9, while: (UInt8(ascii: "0")...UInt8(ascii: "9")).contains)
+            .compactMap { n in Int(Substring(n)).map { $0 * Int(pow(10, 9 - Double(n.count))) } }
         }
-        .map { $0 * ($1 + $2) }
+        .replaceError(with: 0)
+        OneOf {
+          "Z".utf8.map { 0 }
+          Parse {
+            OneOf {
+              "+".utf8.map { 1 }
+              "-".utf8.map { -1 }
+            }
+            Digits(2).filter { $0 < 24 }.map { $0 * 60 * 60 }
+            ":".utf8
+            Digits(2).filter { $0 < 60 }.map { $0 * 60 }
+          }
+          .map { $0 * ($1 + $2) }
+        }
       }
     }
   }
-}
 
-private extension Date {
-  init(
-    year: Int,
-    month: Int,
-    day: Int,
-    hour: Int,
-    minute: Int,
-    second: Int,
-    nanosecond: Int,
-    timeZone: Int
-  ) {
-    var components = tm(
-      tm_sec: Int32(second),
-      tm_min: Int32(minute),
-      tm_hour: Int32(hour),
-      tm_mday: Int32(day),
-      tm_mon: Int32(month - 1),
-      tm_year: Int32(year - 1900),
-      tm_wday: 0,
-      tm_yday: 0,
-      tm_isdst: 0,
-      tm_gmtoff: 0,
-      tm_zone: nil
-    )
-    let time = timegm(&components)
-    var timeIntervalSince1970 = TimeInterval(time - timeZone)
-    timeIntervalSince1970 += TimeInterval(nanosecond) / TimeInterval(NSEC_PER_SEC)
-    self.init(timeIntervalSince1970: timeIntervalSince1970)
+  private extension Date {
+    init(
+      year: Int,
+      month: Int,
+      day: Int,
+      hour: Int,
+      minute: Int,
+      second: Int,
+      nanosecond: Int,
+      timeZone: Int
+    ) {
+      var components = tm(
+        tm_sec: Int32(second),
+        tm_min: Int32(minute),
+        tm_hour: Int32(hour),
+        tm_mday: Int32(day),
+        tm_mon: Int32(month - 1),
+        tm_year: Int32(year - 1900),
+        tm_wday: 0,
+        tm_yday: 0,
+        tm_isdst: 0,
+        tm_gmtoff: 0,
+        tm_zone: nil
+      )
+      let time = timegm(&components)
+      var timeIntervalSince1970 = TimeInterval(time - timeZone)
+      timeIntervalSince1970 += TimeInterval(nanosecond) / TimeInterval(NSEC_PER_SEC)
+      self.init(timeIntervalSince1970: timeIntervalSince1970)
+    }
   }
-}
+#endif
 
 /// This benchmarks implements an [RFC-3339-compliant](https://www.ietf.org/rfc/rfc3339.txt) date
 /// parser in a relatively naive way and pits it against `DateFormatter` and `ISO8601DateFormatter`.
